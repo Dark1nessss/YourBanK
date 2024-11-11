c090d0720e9605ac72340f7baf2569029e5a2b13
Dark1nessss <frankhorn99@gmail.com>
2024-11-11 21:33:21 +0000

    Sidebar Update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import Image from "next/image"
-import Link from "next/link"
+"use client"
+
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const Sidebar = ({user}: SiderbarProps) => {
+
+    const pathname = usePathname();
+
   return (
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
@@ -18,6 +25,22 @@ const Sidebar = ({user}: SiderbarProps) => {
                 YourBanK
                 </h1> 
             </Link>
+
+            {sidebarLinks.map((item) =>{
+
+                const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+
+                return(
+                    <Link href={item.route} key={item.label} className={cn("sidebar-link", { "bg-bank-gradient": isActive })}>
+                        <div className="relative size-6">
+                            <Image src={item.imgURL} alt={item.label} fill className={cn({ "brightness-[3] invert-0": isActive })}/>  
+                        </div>
+                        <p className={cn("sidebar-label", {"!text-white": isActive})}>
+                            {item.label}
+                        </p>
+                    </Link>
+                )
+            })}
         </nav>
     </section>
   )
