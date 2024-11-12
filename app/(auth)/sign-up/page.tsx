48a6ae6648fbdd6675bf481fcb0486433b8b6d39
Dark1nessss <frankhorn99@gmail.com>
2024-11-12 11:58:56 +0000

    Update Auth system

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -4,9 +4,7 @@ import AuthForm from '@/components/AuthForm'
 const SignUp = () => {
   return (
     <section className="flex-center size-full max-sm:px-6">
-      <form>
         <AuthForm type="sign-up"/>
-      </form>
     </section>
   )
 }
