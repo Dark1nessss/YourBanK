4bee403a28c356e5af6927250c2fc38c28d46f37
Tanjil <tanjilkh@gmail.com>
2024-11-30 22:29:25 +0000

    web: custom error 403

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -3,11 +3,25 @@
 import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
 import { useEffect } from "react";
+import ForbiddenPage from "@/components/ForbiddenPage";
+
+
 
 export default function GlobalError({ error }) {
   useEffect(() => {
     Sentry.captureException(error);
   }, [error]);
+
+  // Check for 403 error
+  if (error?.status === 403) {
+    return (
+      <html>
+        <body>
+          <ForbiddenPage />
+        </body>
+      </html>
+    );
+  }
 
   return (
     <html>
