55980cfc18af9d4abc34e9b76114d966e25e4ac7
Dark1nessss <frankhorn99@gmail.com>
2025-01-19 20:25:18 +0000

    Removed repetitive CSS

--- components/CardAuth.tsx
+++ components/CardAuth.tsx
@@ -8,10 +8,10 @@ const CardAuth = () => {
   const mockExpirationDate = "12/12";
 
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [isMounted, setIsMounted] = useState(false); // Track if the component has mounted
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    setIsMounted(true); // Set to true once the component has mounted
+    setIsMounted(true);
 
     const handleMouseMove = (event: MouseEvent) => {
       setMousePosition({ x: event.clientX, y: event.clientY });
@@ -24,20 +24,19 @@ const CardAuth = () => {
     };
   }, []);
 
-  // Only access window if the component is mounted (client-side)
-  const rotationX = isMounted ? (mousePosition.y / window.innerHeight - 0.5) * 60 : 0;
-  const rotationY = isMounted ? (mousePosition.x / window.innerWidth - 0.5) * -60 : 0;
+  const rotationX = isMounted ? (mousePosition.y / window.innerHeight - 0.5) * 50 : 0;
+  const rotationY = isMounted ? (mousePosition.x / window.innerWidth - 0.5) * -50 : 0;
 
   return (
     <div className="flex flex-col justify-center items-center">
       <div
-        className="auth-bank-card relative flex h-[190px] w-full max-w-[320px] justify-between rounded-[20px] border border-white bg-gradient-to-r from-green-500 to-blue-600 shadow-xl backdrop-blur-[6px] transition-all duration-300"
+        className="bank-card"
         style={{
           transform: `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`,
         }}
       >
-        <div className="auth-bank-card_content relative z-10 flex flex-col justify-between rounded-l-[20px] bg-gray-700 bg-opacity-80 px-5 pb-4 pt-5">
-          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+        <div className="bank-card_content">
+          <Image src="/icons/logo_white.svg" width={30} height={30} alt="logo" />
           <h1 className="text-16 font-semibold text-white">YourBank</h1>
 
           <article className="flex flex-col gap-2">
@@ -51,7 +50,7 @@ const CardAuth = () => {
           </article>
         </div>
 
-        <div className="auth-bank-card_icon flex flex-col items-end justify-between rounded-r-[20px] bg-gradient-to-r from-green-500 to-blue-600 bg-cover bg-center py-5 pr-5">
+        <div className="bank-card_icon">
           <Image 
             src="/icons/Paypass.svg"
             width={20}
@@ -72,7 +71,7 @@ const CardAuth = () => {
           width={316}
           height={190}
           alt="Card design"
-          className="auth-bank-card_lines absolute top-0 left-0"
+          className="absolute top-0 left-0"
         />
       </div>
     </div>
