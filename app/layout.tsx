b1c6d4f264c9eb5689c7f7a487c8a9b2e7a72279
Dark1nessss <frankhorn99@gmail.com>
2025-01-19 15:38:35 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
       <ClientWrapper>{children}</ClientWrapper>
       </body>
     </html>
