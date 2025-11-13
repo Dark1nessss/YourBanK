e699d64b605644ff7d5d7756604e4d208c74861a
Dark1nessss <frankhorn99@gmail.com>
2025-11-13 18:57:19 +0000

    Updated signup issues (Must fix date of birth issue later on)

--- lib/utils.ts
+++ lib/utils.ts
@@ -202,17 +202,33 @@ export const getTransactionStatus = (date: Date) => {
 export const authFormSchema = (type: string) =>
   z.object({
     // sign up
-    firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-    lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-    address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+    firstName: type === 'sign-in' ? z.string().optional() : z.string().min(2),
+    lastName: type === 'sign-in' ? z.string().optional() : z.string().min(2),
+    address1: type === 'sign-in' ? z.string().optional() : z.string().max(100),
     city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
     state:
       type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
     postalCode:
-      type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
-    dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-    ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+      type === 'sign-in' ? z.string().optional() : z.string().min(5).max(5), // US ZIP code is 5 digits
+    dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string(), // FIX, must be a date plus needs updating in the frotend
+    ssn: type === 'sign-in' ? z.string().optional() : z.string().min(9).max(9),
     // both
     email: z.string().email(),
-    password: z.string().min(8),
+    password:
+      type === 'sign-up'
+        ? z
+            .string()
+            .min(8, { message: 'Password must be at least 8 characters long' })
+            .refine(value => /[A-Z]/.test(value), {
+              message: 'Password must contain at least one uppercase letter',
+            })
+            .refine(value => /[0-9]/.test(value), {
+              message: 'Password must contain at least one number',
+            })
+            .refine(value => /[!@#$%^&*(),.?":{}|<>]/.test(value), {
+              message: 'Password must contain at least one special character',
+            })
+        : type === 'sign-in'
+          ? z.string().min(8)
+          : z.string(),
   });
